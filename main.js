5394311efa4cb625c66966115165d2769b068473
WilliamsBeck <williams.422024010@civitas.ukrida.ac.id>
2024-12-02 22:36:42 +0700

    Add files via upload
    update landing page - williams

---
+++ main.js
@@ -0,0 +1,5 @@
+// Menunggu sampai halaman selesai dimuat
+window.addEventListener('load', function () {
+    const content = document.querySelector('.container');
+    content.classList.add('show'); // Menambahkan kelas 'show' untuk memulai transisi
+  });
