92a5888e696f991e6470c4f9829cb4203dd8425d
WilliamsBeck <williams.422024010@civitas.ukrida.ac.id>
2024-12-02 22:51:31 +0700

    Delete uas.js

--- uas.js
+++
@@ -1,5 +0,0 @@
-// Menunggu sampai halaman selesai dimuat
-window.addEventListener('load', function () {
-    const content = document.querySelector('.container');
-    content.classList.add('show'); // Menambahkan kelas 'show' untuk memulai transisi
-  });
